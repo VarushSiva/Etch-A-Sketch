1c2450aa388186000ff6c653457530bdc910e743
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-05-24 23:37:12 -0400

    Fix 16x16 Grid cell placement

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector(".grid");
+grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+grid.style.gridTemplateRows = `repeat(16, 1fr)`;
 
 for (let i = 0; i < 256; i++) {
     let cell = document.createElement("div");
