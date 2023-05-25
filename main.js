69acb8ed88b64c3a65e1567b1d70a31fd1d52692
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-05-25 02:14:07 -0400

    Create Grid using Function

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
-grid.style.gridTemplateRows = `repeat(16, 1fr)`;
+function makeGrid(size) {
+  const grid = document.querySelector(".grid");
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
+  let cellCount = size*size;
+
+  for (let i = 0; i < cellCount; i++) {
     let cell = document.createElement("div");
     cell.className = "cell";
     grid.appendChild(cell);
-}
+  }
+};
+
+makeGrid(18);
